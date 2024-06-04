a6cc3fe8bb993577be28b38847a238398911d694
Valentina Duarte <valentinadc1@hotmail.com>
2024-06-04 11:31:18 -0500

    Se agrega la entidad survey

---
+++ src/main/java/com/riwi/prueba/domain/entities/Survey.java
@@ -0,0 +1,53 @@
+package com.riwi.prueba.domain.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.util.List;
+
+@Entity(name = "surveys")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Survey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 255, nullable = false)
+    private String title;
+
+    private String description;
+
+    private LocalDateTime creationDate;
+
+    private Boolean active;
+
+    /* Relacion con user */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    /* Relacion con question */
+    @OneToMany(mappedBy = "text", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Question> questions;
+}
--- src/main/java/com/riwi/prueba/domain/entities/User.java
+++ src/main/java/com/riwi/prueba/domain/entities/User.java
@@ -1,12 +1,19 @@
 package com.riwi.prueba.domain.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
@@ -32,4 +39,10 @@ public class User {
 
     @Column(nullable = false)
     private boolean active;
+
+     /* Relacion con tabla survey */
+    @OneToMany(mappedBy = "user",fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Survey> surveys;
 }
